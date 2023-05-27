e50e89c9a49b846e440602f7be45a87413423e00
Menna2211 <101019260+Menna2211@users.noreply.github.com>
2023-05-27 15:32:34 +0300

    Image-Captioning.py

--- pages/Image-Captioning.py
+++ pages/Image-Captioning.py
@@ -14,10 +14,11 @@ from PIL import Image
 import requests
 from transformers import BlipProcessor, BlipForConditionalGeneration
 
+
 @st.cache(allow_output_mutation=True)
 def get_model():
-    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
-    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
     return processor, model
 
 processor, model =get_model()
