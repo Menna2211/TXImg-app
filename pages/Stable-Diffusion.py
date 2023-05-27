80c8bc9c7c61e4e5315d6ac7fc6ac75a3f89bf00
Menna2211 <101019260+Menna2211@users.noreply.github.com>
2023-05-27 13:10:50 +0300

    Stable-Diffusion.py

--- pages/Stable-Diffusion.py
+++ pages/Stable-Diffusion.py
@@ -14,7 +14,7 @@ import time
 
 model_id = "runwayml/stable-diffusion-v1-5"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
-pipe = pipe.to("cuda")
+#pipe = pipe.to("cuda")
 
 
 st.title("Stable Diffusion App")
